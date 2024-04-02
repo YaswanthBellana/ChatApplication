6f528cb87da05fa43b587f7356e8a6890e23f664
Bellana Yaswanth <yaswanth1847@gmail.com>
2024-04-02 19:19:29 +0530

    almost done

--- client/script.js
+++ client/script.js
@@ -6,12 +6,12 @@ function joinChat() {
 }
 
 socket.on('userList', (users) => {
-    const userList = document.getElementById('userList');
-    userList.innerHTML = ''; // Clear the previous options
-    for (const userId in users) {
+    const userList = document.getElementById('recipient');
+    userList.innerHTML = '<option value="">Select a user</option>';
+    for (const user of users) {
         const option = document.createElement('option');
-        option.value = userId;
-        option.text = users[userId];
+        option.value = user;
+        option.text = user;
         userList.appendChild(option);
     }
 });
@@ -22,23 +22,17 @@ function sendMessage() {
     if (recipientUsername !== '') {
         console.log(`Sending message "${msg}" to recipient ${recipientUsername}`);
         socket.emit('chatMessage', msg, recipientUsername);
+        // Clear the input field after sending message
+        document.getElementById('messageInput').value = '';
     } else {
         console.log("No recipient selected.");
     }
 }
 
 socket.on('chatMessage', (msg, senderUsername) => {
-    const decryptedMsg = decryptMessage(msg);
-    console.log(`Received message from ${senderUsername}: ${decryptedMsg}`);
-    displayMessage(`${senderUsername}: ${decryptedMsg}`);
+    console.log(`Received message from ${senderUsername}: ${msg}`);
+    displayMessage(`${senderUsername}: ${msg}`);
 });
-
-function decryptMessage(msg) {
-    const decipher = crypto.createDecipher('aes-256-cbc', 'mySecretKey');
-    let decrypted = decipher.update(msg, 'hex', 'utf8');
-    decrypted += decipher.final('utf8');
-    return decrypted;
-}
 
 function displayMessage(message) {
     const messagesContainer = document.getElementById('messages');
